2dad18c1fc40b19bf756180f2f2c9985c47990f3
Ekweaga <hikay133@gmail.com>
2022-08-10 21:00:41 +0100

    Home page design in progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <Home/>
     </div>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import './home.css'
+import imginvoice from './assets/invoice.jpg'
+
+function Home() {
+  return (
+    <>
+    <div className='header'>
+        <div className="menu">
+        <Navbar/>
+        </div>
+
+        <div className='headerContainer '>
+            <div className='headertitle'>
+                <div>
+                    <h1>Send online invoices to different clients and customers</h1>
+                </div>
+                <div><p>Professional and Easy-to-use Online Invoicing Software for Startups, Business Owners and Freelancers.
+                    </p></div>
+
+                    <div className='headerbtn'><button>Create Free Account</button></div>
+            </div>
+
+
+            <div className="headerimg">
+                        <div>
+                            <img src={imginvoice}/>
+                        </div>
+            </div>
+        </div>
+     
+    </div>
+
+
+    <div className="benefitsContainer">
+        <div className='benefits'>
+            <div>
+                <h3>Save more time</h3>
+            </div>
+            <div>
+            <h3>Save more time</h3>
+            </div>
+            <div>
+            <h3>Easy to Use</h3>
+            </div>
+        </div>
+
+    </div>
+
+    </>
+  )
+}
+
+export default Home
